adeb3cc8fadd0194d6cac04bb34c1a1fe1b0244a
Swapnil Bhandari <swapnil20222@gmail.com>
2025-12-06 00:19:52 +0545

    Changed api.js to work in vercel

--- purplebay-frontend/src/services/api.js
+++ purplebay-frontend/src/services/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}/api`, // <-- using env variable
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: { "Content-Type": "application/json" },
+  withCredentials: true, // 
 });
 
-// Attach token if exists
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
