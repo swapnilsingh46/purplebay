bb66ea4e9b977d5768df8ebb7a6e9cb62495186f
Swapnil Bhandari <swapnil20222@gmail.com>
2025-12-05 23:29:39 +0545

    Prep for Vercel

--- purplebay-frontend/src/services/api.js
+++ purplebay-frontend/src/services/api.js
@@ -1,14 +1,13 @@
-// src/services/api.js
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // backend URL
-  headers: { 'Content-Type': 'application/json' },
+  baseURL: `${import.meta.env.VITE_API_URL}/api`, // <-- using env variable
+  headers: { "Content-Type": "application/json" },
 });
 
 // Attach token if exists
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
